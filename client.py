4049b9fb40e915b6c15a6a46ef5dca1cd98a6e6a
Enygma <57713995+Aswin-A-Arun@users.noreply.github.com>
2022-05-05 21:37:44 +0530

    Update client.py

--- client.py
+++ client.py
@@ -28,7 +28,32 @@ global song_counter
 song_counter = 0
 
 
+def browseFiles():
+    global listbox
+    global song_counter
+    global filePathLabel
 
+    try:
+        filename = filedialog.askopenfilename()
+        HOSTNAME = "127.0.0.1"
+        USERNAME = "lftpd"
+        PASSWORD = "lftpd"
+
+        ftp_server = FTP(HOSTNAME, USERNAME, PASSWORD)
+        ftp_server.encoding = "utf-8"
+        ftp_server.cwd('shared_files')
+        fname=ntpath.basename(filename)
+        with open(filename, 'rb') as file:
+            ftp_server.storbinary(f"STOR {fname}", file)
+
+        ftp_server.dir()
+        ftp_server.quit()
+       
+        listbox.insert(song_counter, fname)
+        song_counter = song_counter + 1
+        
+    except FileNotFoundError:
+        print("Cancel Button Pressed")
 
 
 def play():
@@ -51,8 +76,7 @@ def stop():
     mixer.music.load('shared_files/'+song_selected)
     mixer.music.pause()
     infoLabel.configure(text="")
-
-
+    
 def pause():
     global song_selected
     pygame
@@ -64,11 +88,34 @@ def resume():
     global song_selected
     mixer.init()
     mixer.music.load('shared_files/'+song_selected)
-    mixer.music.play()
-    mixer.music.unpause() 
-
+    mixer.music.play() 
    
-
+  
+def download():    
+    song_to_download=listbox.get(ANCHOR)
+    infoLabel.configure(text="Downloading "+ song_to_download)
+    HOSTNAME = "127.0.0.1"
+    USERNAME = "lftpd"
+    PASSWORD = "lftpd"
+    home = str(Path.home())
+    download_path=home+"/Downloads"
+    ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
+    ftp_server.encoding = "utf-8"
+    ftp_server.cwd('shared_files')
+    local_filename = os.path.join(download_path, song_to_download)
+    file = open(local_filename, 'wb')
+    ftp_server.retrbinary('RETR '+ song_to_download, file.write)
+    ftp_server.dir()
+    file.close()
+    ftp_server.quit()
+    infoLabel.configure(text="Download Complete")
+    time.sleep(1)
+    if(song_selected != ""):
+        infoLabel.configure(text="Now Playing: " +song_selected)
+    else:
+       infoLabel.configure(text="") 
+   
+   
 def musicWindow(): 
     global song_counter
     global filePathLabel
@@ -77,7 +124,7 @@ def musicWindow():
     
     window=Tk()
     window.title('Music Window')
-    window.geometry("300x350")
+    window.geometry("300x300")
     window.configure(bg='LightSkyBlue')
     
     selectlabel = Label(window, text= "Select Song",bg='LightSkyBlue', font = ("Calibri",8))
@@ -99,17 +146,22 @@ def musicWindow():
     
     Stop=Button(window,text="Stop",bd=1,width=10,bg='SkyBlue', font = ("Calibri",10), command = stop)
     Stop.place(x=200,y=200)
+    
+     ResumeButton=Button(window,text="Resume", width=10,bd=1,bg='SkyBlue',font = ("Calibri",10), command = resume)
+    ResumeButton.place(x=30,y=250)
 
-       
+    PauseButton=Button(window,text="Pause", width=10,bd=1,bg='SkyBlue',font = ("Calibri",10), command = pause)
+    PauseButton.place(x=200,y=250)
+
     
-    Upload=Button(window,text="Upload",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10))
-    Upload.place(x=30,y=300)
+    Upload=Button(window,text="Upload",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10), command = browseFiles)
+    Upload.place(x=30,y=250)
     
-    Download =Button(window,text="Download",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10))
-    Download.place(x=200,y=300)
+    Download =Button(window,text="Download",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10), command = download)
+    Download.place(x=200,y=250)
     
     infoLabel = Label(window, text= "",fg= "blue",bg='SkyBlue', font = ("Calibri",8))
-    infoLabel.place(x=4, y=330)
+    infoLabel.place(x=4, y=280)
     
     window.mainloop()
     
@@ -123,7 +175,12 @@ def setup():
     SERVER.connect((IP_ADDRESS, PORT))
 
     musicWindow()
-   
+
 setup()
+
+
+   
+
+
 
 
